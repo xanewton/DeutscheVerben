c1e2178aadbc9e32d4553d423ad14a4c00aeed47
Angel Newton <xanewton@gmail.com>
2018-06-14 13:44:32 -0400

    Add Help functionality

--- app/src/main/java/com/xengar/android/deutscheverben/ui/HelpActivity.kt
+++ app/src/main/java/com/xengar/android/deutscheverben/ui/HelpActivity.kt
@@ -15,28 +15,188 @@
  */
 package com.xengar.android.deutscheverben.ui
 
+import android.app.Dialog
+import android.content.Context
+import android.content.Intent
+import android.graphics.Rect
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import com.xengar.android.deutscheverben.R
+import android.support.v7.widget.Toolbar
+import android.util.TypedValue
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
 
-import kotlinx.android.synthetic.main.activity_help.*
+//import com.google.firebase.analytics.FirebaseAnalytics
+import com.xengar.android.deutscheverben.BuildConfig
+import com.xengar.android.deutscheverben.R
+import com.xengar.android.deutscheverben.utils.ActivityUtils
+
+import com.xengar.android.deutscheverben.utils.Constants.PAGE_HELP
+import com.xengar.android.deutscheverben.utils.Constants.TYPE_CONTEXT_HELP
+import com.xengar.android.deutscheverben.utils.Constants.TYPE_PAGE
 
 /**
  * HelpActivity
  */
-class HelpActivity : AppCompatActivity() {
+class HelpActivity : AppCompatActivity(), View.OnClickListener {
+
+    //private var mFirebaseAnalytics: FirebaseAnalytics? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_help)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        // define click listeners
+        val header = findViewById<LinearLayout>(R.id.header_verbs)
+        header.setOnClickListener(this)
+
+        // Change text size
+        val fontSize = Integer.parseInt(ActivityUtils.getPreferenceFontSize(applicationContext))
+        (findViewById<View>(R.id.description_main) as TextView).setTextSize(TypedValue.COMPLEX_UNIT_SP, fontSize.toFloat())
+        (findViewById<View>(R.id.description_volume) as TextView).setTextSize(TypedValue.COMPLEX_UNIT_SP, fontSize.toFloat())
+
+        // Obtain the FirebaseAnalytics instance.
+        //mFirebaseAnalytics = FirebaseAnalytics.getInstance(applicationContext)
+        //ActivityUtils.firebaseAnalyticsLogEventSelectContent(mFirebaseAnalytics!!,
+        //        PAGE_HELP, PAGE_HELP, TYPE_PAGE)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.help, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item.itemId
+
+        when (id) {
+            R.id.action_feedback -> {
+                giveFeedback()
+                return true
+            }
+
+            R.id.action_problem -> {
+                reportProblem()
+                return true
+            }
+
+            R.id.action_license -> {
+                showLicense()
+                return true
+            }
+
+            R.id.action_version -> {
+                showVersion()
+                return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+
+    /**
+     * Send feedback email.
+     */
+    private fun reportProblem() {
+        val sendMessage = Intent(Intent.ACTION_SEND)
+        sendMessage.type = "message/rfc822"
+        sendMessage.putExtra(Intent.EXTRA_EMAIL, arrayOf(resources.getString(R.string.feedback_email)))
+        sendMessage.putExtra(Intent.EXTRA_SUBJECT, "Deutsche Verben Problem")
+        sendMessage.putExtra(Intent.EXTRA_TEXT,
+                resources.getString(R.string.problem_message))
+        try {
+            startActivity(Intent.createChooser(sendMessage, "Report a problem"))
+        } catch (e: android.content.ActivityNotFoundException) {
+            Toast.makeText(applicationContext, "Communication app not found",
+                    Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    /**
+     * Send feedback email.
+     */
+    private fun giveFeedback() {
+        val sendMessage = Intent(Intent.ACTION_SEND)
+        sendMessage.type = "message/rfc822"
+        sendMessage.putExtra(Intent.EXTRA_EMAIL, arrayOf(resources.getString(R.string.feedback_email)))
+        sendMessage.putExtra(Intent.EXTRA_SUBJECT, "Deutsche Verben Feedback")
+        sendMessage.putExtra(Intent.EXTRA_TEXT,
+                resources.getString(R.string.feedback_message))
+        try {
+            startActivity(Intent.createChooser(sendMessage, "Give Feedback"))
+        } catch (e: android.content.ActivityNotFoundException) {
+            Toast.makeText(applicationContext, "Communication app not found",
+                    Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    /**
+     * Show version information
+     */
+    private fun showVersion() {
+        //set up dialog
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.version_dialog)
+        dialog.setCancelable(true)
+
+        //set up text
+        val text = dialog.findViewById<TextView>(R.id.version_number)
+        text.text = BuildConfig.VERSION_NAME
+
+        dialog.show()
+    }
+
+    /**
+     * Show License information
+     */
+    private fun showLicense() {
+        // retrieve display dimensions
+        val displayRectangle = Rect()
+        val window = window
+        window.decorView.getWindowVisibleDisplayFrame(displayRectangle)
+
+        val inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val layout = inflater.inflate(R.layout.license_dialog, null)
+        layout.minimumWidth = (displayRectangle.width() * 0.75f).toInt()
+        layout.minimumHeight = (displayRectangle.height() * 0.75f).toInt()
+
+        //set up dialog
+        val dialog = Dialog(this)
+        dialog.setContentView(layout)
+        dialog.setCancelable(true)
+
+        //set up text
+        val text = dialog.findViewById<TextView>(R.id.copyright)
+        text.text = ActivityUtils.fromHtml(getString(R.string.eula_string))
+
+        dialog.show()
+    }
+
+    override fun onClick(view: View) {
+        when (view.id) {
+            R.id.header_verbs -> {
+                // dormir
+                ActivityUtils.launchDetailsActivity(applicationContext, 100, 33, "dormir", true)
+                //ActivityUtils.firebaseAnalyticsLogEventSelectContent(mFirebaseAnalytics!!,
+                //        "Contextual help", "acheter", TYPE_CONTEXT_HELP)
+            }
+        }
     }
 
 }
